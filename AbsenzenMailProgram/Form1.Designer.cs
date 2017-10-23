995ea0bba8acbc3e00ac898c89d2ce2a42741323
Hannes Eybl <hannes.eybl@stud.gymhofwil.ch>
2017-10-23 19:12:33 +0200

    Version vom Labtop - Ursprünglich

---
+++ AbsenzenMailProgram/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace AbsenzenMailProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b_newbox = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // b_newbox
+            // 
+            this.b_newbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b_newbox.Location = new System.Drawing.Point(40, 80);
+            this.b_newbox.Name = "b_newbox";
+            this.b_newbox.Size = new System.Drawing.Size(26, 26);
+            this.b_newbox.TabIndex = 0;
+            this.b_newbox.Text = "+";
+            this.b_newbox.UseVisualStyleBackColor = true;
+            this.b_newbox.Click += new System.EventHandler(this.b_newbox_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.b_newbox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b_newbox;
+    }
+}
+
---
+++ AbsenzenMailProgram/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AbsenzenMailProgram
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CreateCbox();
+        }
+                
+        private static int CboxCounter = 0;
+        private void CreateCbox()
+        {
+            //Set up Comboboxes
+            ComboBox cBox1 = new ComboBox();
+            ComboBox cBox2 = new ComboBox();
+            ComboBox cBox3 = new ComboBox();
+            ComboBox cBox4 = new ComboBox();
+            ComboBox cBox5 = new ComboBox();
+            ComboBox cBox6 = new ComboBox();
+            ComboBox cBox7 = new ComboBox();
+            ComboBox cBox8 = new ComboBox();
+            ComboBox[] cBoxArray = { cBox1, cBox2, cBox3, cBox4, cBox5, cBox6, cBox7, cBox8 };
+
+            //Set Location for new Combobox and set new Location for Button
+            cBoxArray[CboxCounter].Location = new Point(b_newbox.Location.X, b_newbox.Location.Y);
+            b_newbox.Location = new Point(b_newbox.Location.X, b_newbox.Location.Y + 20);
+
+            //Add new Combobox to Frame
+            this.Controls.Add(cBoxArray[CboxCounter]);
+
+            CboxCounter += 1;
+
+        }
+
+        private void b_newbox_Click(object sender, EventArgs e)
+        {
+            CreateCbox();
+        }
+    }
+
+}
